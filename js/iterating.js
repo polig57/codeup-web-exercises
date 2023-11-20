0296907e7d3f781b87b18e24d1964e7bba7465d0
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-11-20 09:14:43 -0600

    Finished iterating exercise

--- js/iterating.js
+++ js/iterating.js
@@ -18,18 +18,20 @@ console.log(names.length);
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-console.log(names.forEach(el))
+console.log(...names);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+for(let name of names) {
+    console.log(name);
+}
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+names.forEach(name => console.log(name));
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +45,21 @@ console.log(names.forEach(el))
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-
+const first = (arr) => {
+  return arr[0];
+}
+// or this way works
+// function first(arr) {
+//     return arr[0];
+// }
+const second = (arr) => {
+  return arr[1];
+}
+const last = (arr) => {
+  return arr[arr.length - 1];
+}
+    console.log(first([1, 2, 3, 4, 5]));
+    console.log(second([1, 2, 3, 4, 5]));
+    console.log(last([1, 2, 3, 4, 5]));
 })();
+
