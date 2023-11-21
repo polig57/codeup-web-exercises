cac8f4f9c3c1c0534766e2d927c1b79f3c91c251
Poli Gonzalez <gonzalezpoli10@gmail.com>
2023-11-21 11:17:48 -0600

    Completed Math exercise

---
+++ js/toy-problems.js
@@ -0,0 +1,15 @@
+function flickSwitch(array){
+    // Create an accumulator that will hold the true or false values
+let accumulator = '';
+    // Create a variable to hold what our previous boolean value was
+let switcher = true;
+
+    for(const word of array) {
+        // check to see if the word is 'flick'
+        if(word === '-') {
+
+        }
+    }
+
+    // Return the accumulator
+}
